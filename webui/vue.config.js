77feaee70a014a44f707be0b415f361e84b1fdce
caidy <vcsdk2468@gmail.com>
2023-05-04 17:24:10 +0800

    remove sourcemap for production

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/ui/',
   transpileDependencies: true,
+  productionSourceMap: false,
   devServer: {
     proxy: {
       "/api": {
